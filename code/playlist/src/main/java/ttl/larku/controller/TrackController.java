837f4f9805e6d06f733d3aced58fa56373e31eae
Aravindakumar V M <aravindakumar_vijayasrimohankumar@intuit.com>
2024-02-14 15:01:57 -0800

    day 2 - lab activity to add response entity to playlist app

--- code/playlist/src/main/java/ttl/larku/controller/TrackController.java
+++ code/playlist/src/main/java/ttl/larku/controller/TrackController.java
@@ -2,10 +2,13 @@ package ttl.larku.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ttl.larku.domain.Track;
 import ttl.larku.service.TrackService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,17 +19,46 @@ public class TrackController {
     private TrackService trackService;
 
     @GetMapping
-    public List<Track> getAllTracks(){
-        return trackService.getAllTracks();
+    public ResponseEntity<List<Track>> getAllTracks(){
+        return ResponseEntity.ok(trackService.getAllTracks());
     }
 
     @GetMapping("/{tid}")
-    public Track getTrack(@PathVariable(name="tid") int tid){
-        return trackService.getTrack(tid);
+    public ResponseEntity<?> getTrack(@PathVariable(name="tid") int tid){
+        Track track = trackService.getTrack(tid);
+        if (track == null) {
+            return ResponseEntity.badRequest().body("No track with id: " + tid);
+        }
+        return ResponseEntity.ok(track);
     }
 
-    @PostMapping("/create")
-    public Track createTrack(@RequestBody Track track){
-        return trackService.createTrack(track);
+    @PostMapping
+    public ResponseEntity<?> createTrack(@RequestBody Track track){
+        Track newTrack = trackService.createTrack(track);
+        URI newResource = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newTrack.getId())
+                .toUri();
+        return ResponseEntity.created(newResource).build();
     }
+
+    @DeleteMapping("/{tid}")
+    public ResponseEntity<?> deleteTrack(@PathVariable(name="tid") int tid){
+        boolean result = trackService.deleteTrack(tid);
+        if (!result) {
+            return ResponseEntity.badRequest().body("No track with id: " + tid);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{tid}")
+    public ResponseEntity<?> updateTrack(@PathVariable(name="tid") int tid, @RequestBody Track toUpdate){
+        boolean result = trackService.updateTrack(toUpdate);
+        if (!result) {
+            return ResponseEntity.badRequest().body("No track with id: " + tid);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 }
