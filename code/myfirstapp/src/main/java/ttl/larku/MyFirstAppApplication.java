7c43266e9c12e37b400caa7d33da387d80a24ea3
Aravindakumar V M <aravindakumar_vijayasrimohankumar@intuit.com>
2024-02-14 11:17:00 -0800

    day 2 - integrate student app with my first app

--- code/myfirstapp/src/main/java/ttl/larku/MyFirstAppApplication.java
+++ code/myfirstapp/src/main/java/ttl/larku/MyFirstAppApplication.java
@@ -1,9 +1,14 @@
-package com.example.myfirstapp;
+package ttl.larku;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import ttl.larku.domain.Student;
+import ttl.larku.service.StudentService;
+
+import java.util.List;
 
 @SpringBootApplication
 public class MyFirstAppApplication {
@@ -17,8 +22,16 @@ public class MyFirstAppApplication {
 @Component
 class MyRunner implements CommandLineRunner{
 
+	@Autowired
+	private StudentService studentService;
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello! from my commandline runner..!");
+		List<Student> students = studentService.getAllStudents();
+		System.out.println("Student size: " + students.size());
+		for (Student student:  students) {
+			System.out.println(student);
+		}
 	}
 }
