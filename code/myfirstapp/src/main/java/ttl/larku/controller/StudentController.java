d61d285887b218397ec720ebdf0af0da1a549292
Aravindakumar V M <aravindakumar_vijayasrimohankumar@intuit.com>
2024-02-14 14:46:26 -0800

    day 2 - response entity addition to myfirstapp

--- code/myfirstapp/src/main/java/ttl/larku/controller/StudentController.java
+++ code/myfirstapp/src/main/java/ttl/larku/controller/StudentController.java
@@ -1,10 +1,13 @@
 package ttl.larku.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ttl.larku.domain.Student;
 import ttl.larku.service.StudentService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,12 +23,42 @@ public class StudentController {
     }
 
     @GetMapping("/{sid}")
-    public Student getStudent(@PathVariable(name="sid") int sid) {
-        return studentService.getStudent(sid);
+    public ResponseEntity<?> getStudent(@PathVariable(name="sid") int sid) {
+        Student student = studentService.getStudent(sid);
+        if (student == null){
+            return ResponseEntity.badRequest().body("No student with id: " + sid);
+        }
+        return ResponseEntity.ok(student);
     }
 
-    @PostMapping("/create")
-    public Student createStudent(@RequestBody Student student){
-        return studentService.createStudent(student);
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student student){
+        Student newStudent = studentService.createStudent(student);
+
+        // http://localhost:8080/students/:id
+        URI newResource = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newStudent.getId())
+                .toUri();
+        return ResponseEntity.created(newResource).build();
+    }
+
+    @DeleteMapping("/{sid}")
+    public ResponseEntity<?> deleteStudent(@PathVariable(name="sid") int sid){
+        boolean result = studentService.deleteStudent(sid);
+        if (!result){
+            return ResponseEntity.badRequest().body("No student with id: " + sid);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{sid}")
+    public ResponseEntity<?> updateStudent(@PathVariable(name="sid") int sid, @RequestBody Student student){
+        boolean result = studentService.updateStudent(student);
+        if (!result){
+            return ResponseEntity.badRequest().body("No student with id: " + sid);
+        }
+        return ResponseEntity.noContent().build();
     }
 }
